aa9fa51bd58a67d378c3e3dd47435bc5943dd62b
UlisesFG98 <ulises.fg.98@gmail.com>
2025-07-18 14:59:40 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 Esto es lo importante
   plugins: [
     vue(),
     //vueDevTools(),
